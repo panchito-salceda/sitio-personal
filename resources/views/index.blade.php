6a54364e9547828596458a0003a6ed33c61b47bd
root <root@COMUNIDADLEON>
2024-06-28 10:45:18 -0600

    Nueva version de index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <nav class="navbar d-sm-inline-flex justify-content-around">
+            <a class="text-decoration-none" href="/">
+                <span class="text-uppercase">Inicio</span></a>
+            <a class="text-decoration-none" href="">
+                <span class="text-uppercase">Galeria</span>
+            </a>
+            <a class="text-decoration-none" href="">
+                <span class="text-uppercase">Tienda</span></a>
+            <a class="text-decoration-none" href="">
+                <span class="text-uppercase">Contacto</span></a>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <h1 class="text-bold">Panchito Salceda</h1>
+        </div>
+    </main>
+    
+    <div class="container w-75 px-5 bg-light">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, saepe est! Esse recusandae voluptatem, nostrum nesciunt debitis, quae quas amet quaerat corrupti error accusamus quis excepturi reiciendis illum culpa officiis.</p>
+    </div>
+    <h2>Ilustrador autodidacta, desarrollador web, todologo por ocio</h2>
+<footer>
+    <div class="container bg-secondary"><p class=text-white>"Panchito Salceda | haciendole a la dibujada | Curtiendo a León " son marcas registradas, León Guanajuato</p></div>
+</footer>
+</body>
+</html>
